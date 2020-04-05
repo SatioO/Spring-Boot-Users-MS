d3fdb92fab9a82dfc6f306439ba0368001a07cdf
SatioO <vaibhav.satam29991@gmail.com>
2020-04-05 14:34:19 +0530

    fix(Controller)

--- src/main/java/com/learn/users/controllers/Users.java
+++ src/main/java/com/learn/users/controllers/Users.java
@@ -3,6 +3,8 @@ package com.learn.users.controllers;
 import com.learn.users.dto.UserDto;
 import com.learn.users.services.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,17 +18,17 @@ public class Users {
     private IUserService UserService;
 
     @GetMapping(path = "/{id}")
-    public UserDto getUser(@PathVariable UUID id) {
-        return UserService.getUserById(id);
+    public ResponseEntity<UserDto> getUser(@PathVariable UUID id) {
+        return new ResponseEntity<>(UserService.getUserById(id), HttpStatus.OK);
     }
 
     @GetMapping
-    public Collection<UserDto> getUsers() {
-        return UserService.getAllUsers();
+    public ResponseEntity<Collection<UserDto>> getUsers() {
+        return new ResponseEntity<>(UserService.getAllUsers(), HttpStatus.OK);
     }
 
     @PostMapping
-    public UserDto createUser(@RequestBody @Valid UserDto user) {
-        return UserService.createUser(user);
+    public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserDto user) {
+        return new ResponseEntity<>(UserService.createUser(user), HttpStatus.OK);
     }
 }
