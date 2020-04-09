c10a82cb1f4d8f290a516062c8fa9521d76dcfd2
SatioO <vaibhav.satam29991@gmail.com>
2020-04-09 12:02:42 +0530

    added json property example

--- src/main/java/com/learn/users/dto/models/BookDTO.java
+++ src/main/java/com/learn/users/dto/models/BookDTO.java
@@ -3,6 +3,7 @@ package com.learn.users.dto.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import lombok.experimental.Accessors;
 
@@ -20,5 +21,6 @@ public class BookDTO {
     private String name;
 
     @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String userId;
 }
