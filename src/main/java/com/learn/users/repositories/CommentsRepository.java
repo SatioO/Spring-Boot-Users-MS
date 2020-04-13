9709f2e7975cc6cc1465f1b5babb4e4c9af7dc6a
SatioO <vaibhav.satam29991@gmail.com>
2020-04-13 14:43:30 +0530

    fix(DTO)

--- src/main/java/com/learn/users/repositories/CommentsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.learn.users.repositories;
-
-import com.learn.users.entities.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentsRepository extends JpaRepository<Comment, Long> {
-}
--- src/main/java/com/learn/users/repositories/PostsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.learn.users.repositories;
-
-import com.learn.users.entities.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PostsRepository extends JpaRepository<Post, Long> {
-}
